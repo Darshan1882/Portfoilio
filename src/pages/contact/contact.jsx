2c245a9241b125de700a8f850cbe75b68d6a9362
Darshan1882 <pateldarshan.frontendarmy@email.com>
2022-11-09 12:08:58 +0530

    update

--- src/pages/contact/contact.jsx
+++ src/pages/contact/contact.jsx
@@ -154,7 +154,7 @@ const Contact = () => {
                 type="submit"
                 onClick={onSubmit}
               >
-                submit
+                Submit
               </button>
             )}
           </div>
